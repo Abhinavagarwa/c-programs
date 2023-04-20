41617cf173871b75f02813180bf9c390220618a3
Abhinavagarwa <119868907+Abhinavagarwa@users.noreply.github.com>
2023-04-20 06:44:43 +0530

    college work

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main()
+    {
+       int i;
+       printf("enter a number    \n");
+       scanf("%d",&i);
+       if(i%2==0)
+       printf("no is even");
+       else
+       printf("no is odd");
+       return 0;
+    }
