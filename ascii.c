f94af6e54535683976b5c5767c7ef300aca766d0
Abhinavagarwa <119868907+Abhinavagarwa@users.noreply.github.com>
2023-04-20 06:54:48 +0530

    college work

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main()
+  {
+    char ch;
+    printf("enter a character \n");
+    scanf("%c",&ch);
+    printf("the ascii value of the inputed character is %d",ch);
+    return 0;
+}
